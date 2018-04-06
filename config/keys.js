c54a6fa816111b5f81215d09bf172b63a77db3be
mahadikrohini <mahadikrohini@gmail.com>
2018-04-07 01:03:05 +0530

    Delete keys.js
    No use

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-// add this file to .gitignore
-
-module.exports = {
-        google: {
-            clientID: '739895745499-5paasbg67jq2olmst222vl2uvnankqem.apps.googleusercontent.com',
-            clientSecret: 'YlOB3_pf6QBbcnANmX07WHmg'
-        }
-};
