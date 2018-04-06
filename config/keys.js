b5419bf80981ca53e9a66752047ae5200bba8a16
mahadikrohini <mahadikrohini@gmail.com>
2018-04-07 00:54:26 +0530

    Basic setup to get profile information by using passport js

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '739895745499-5paasbg67jq2olmst222vl2uvnankqem.apps.googleusercontent.com',
+            clientSecret: 'YlOB3_pf6QBbcnANmX07WHmg'
+        }
+};
